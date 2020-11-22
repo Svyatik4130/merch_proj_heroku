cfb10bb52aa6fe60004239ad434b203646842c11
Svyatik4130 <barberbi1@icloud.com>
2020-11-22 12:54:46 +0200

    fix inf loop

--- front/src/components/pages/PersonalInfo.js
+++ front/src/components/pages/PersonalInfo.js
@@ -22,7 +22,7 @@ export default function PersonalInfo() {
         }
         getUser()
         console.log(userInfo)
-    })
+    }, [])
     if (!isLoaded) {
         return (
             <div className="container">
